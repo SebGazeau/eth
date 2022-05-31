ad5318d8a2b5aac80fc598cf1090c235a0745c2f
sebastien <sebastiengazeau4@gmail.com>
2022-06-01 00:36:54 +0200

    testing characters

--- test/CharactersTest.ts
+++ test/CharactersTest.ts
@@ -23,6 +23,8 @@ contract('Characters', (accounts: Truffle.Accounts) => {
 	const characteristics = characteristicsJSON;
 	const choices = choicesJSON;
 	const tokenURI = 'token uri';
+	const tokenURIstone = 'token uri tombstone';
+	const tokenURIend = 'token uri end';
 	const namePersonage = 'Morwan'
 	let CharactersInstance: CharactersInstance;
 	let personage:ttp, proposal: ttp, vote: ttp, tally: ttp, status: ttp;
@@ -44,7 +46,7 @@ contract('Characters', (accounts: Truffle.Accounts) => {
 			const leafChoice = keccak256(leaveChoice[0]);
 			const proofChoice = treeChoice.getHexProof(leafChoice);
 			it('Mint Personage',async () => {
-				personage = await CharactersInstance.MintPersonage(account.first, tokenURI, namePersonage, rootCharacteristic, rootChoice);
+				personage = await CharactersInstance.firstPersonage(tokenURI, namePersonage, rootCharacteristic, rootChoice, {from: account.first});
 				expect(personage).to.be.ok;
 			});
 			it('get Event Mint', () => {
@@ -53,29 +55,63 @@ contract('Characters', (accounts: Truffle.Accounts) => {
 			it('should verify choice', async () => {
 				console.log('leafChoice',leafChoice)
 				console.log('proofChoice',proofChoice)
-				expect(await CharactersInstance.madeIsChoice('left',proofChoice, new BN(0))).to.equal(true);
+				expect(await CharactersInstance.madeIsChoice( new BN(0),'left',proofChoice)).to.equal(true);
 			});
 			it('should verify Characteristic', async () => {
-				expect(await CharactersInstance.hasThisCharacteristic('red',proofCharacteristic, new BN(0))).to.equal(true);
+				expect(await CharactersInstance.hasThisCharacteristic(new BN(0),'red',proofCharacteristic)).to.equal(true);
 			});
-		});
-		context('in progress', () => {
-			const tabChoices: any[] = [];
-			const newChoices = [...choices,{choicesThree: 'middle'}];
-			newChoices.map(choice => {tabChoices.push(Object.values(choice)[0])});
-			const leaveChoice = tabChoices.map(choice => choice)
-			const treeChoice = new MerkleTree(leaveChoice, keccak256, { hashLeaves: true, sortPairs: true });
-			const rootChoice = treeChoice.getHexRoot();
-			const leafChoice = keccak256('middle');
-			const proofChoice = treeChoice.getHexProof(leafChoice);
-			it('Add new merkle tree',async () => {
-				personage = await CharactersInstance.setProofOfChoices(new BN(0),rootChoice);
+			it('must not create a new first character', async () =>{
+				await expectRevert(CharactersInstance.firstPersonage(tokenURI, namePersonage, rootCharacteristic, rootChoice, {from: account.first}), "Use other function");
+			});
+			it('must not create another character', async () =>{
+				await expectRevert(CharactersInstance.newAfterDeath(tokenURI, namePersonage, rootCharacteristic, rootChoice, {from: account.first}), "A character is not dead");
+			});
+			it('must kill a character', async () =>{
+				personage = await CharactersInstance.deadPersonage(new BN(0),tokenURIstone, {from: account.first});
+				expect(personage).to.be.ok;
+				const all =await CharactersInstance.getAllPersonages();
+				console.log('all',all)
+			});
+			context('should not be able to modify a dead character', ()=>{
+				const newChoices = [...choices,{choicesThree: 'middle'}];
+				newChoices.map(choice => {tabChoices.push(Object.values(choice)[0])});
+				const leaveChoice = tabChoices.map(choice => choice)
+				const treeChoice = new MerkleTree(leaveChoice, keccak256, { hashLeaves: true, sortPairs: true });
+				const rootChoice = treeChoice.getHexRoot();
+				it('do not add new evidence to a grave', async () =>{
+					await expectRevert(CharactersInstance.setProofOfChoices(new BN(0),rootChoice, {from: account.first}), "character can't be used");
+				});
+			})
+			it('should create a new character when all others are dead', async () =>{
+				personage = await CharactersInstance.newAfterDeath(tokenURI, namePersonage, rootCharacteristic, rootChoice, {from: account.first});
 				expect(personage).to.be.ok;
 			});
-			it('should verify choice', async () => {
-				console.log('proofChoice',proofChoice)
-				expect(await CharactersInstance.madeIsChoice('middle',proofChoice, new BN(0))).to.equal(true);
+			it('get Event Mint for new character', () => {
+				expectEvent(personage, 'Transfer',{from: account.zero, to: account.first, tokenId: new BN(1)})
 			});
-		})
+			context('in progress', () => {
+				const tabChoices: any[] = [];
+				const newChoices = [...choices,{choicesThree: 'middle'}];
+				newChoices.map(choice => {tabChoices.push(Object.values(choice)[0])});
+				const leaveChoice = tabChoices.map(choice => choice)
+				const treeChoice = new MerkleTree(leaveChoice, keccak256, { hashLeaves: true, sortPairs: true });
+				const rootChoice = treeChoice.getHexRoot();
+				const leafChoice = keccak256('middle');
+				const proofChoice = treeChoice.getHexProof(leafChoice);
+				it('Add new merkle tree',async () => {
+					personage = await CharactersInstance.setProofOfChoices(new BN(1),rootChoice, {from: account.first});
+					expect(personage).to.be.ok;
+				});
+				it('should verify choice', async () => {
+					expect(await CharactersInstance.madeIsChoice(new BN(1),'middle',proofChoice)).to.equal(true);
+				});
+			});
+			context('for the end', () => {
+				it('should create the final character', async () => {
+					personage = await CharactersInstance.endOfStoryPersonage(new BN(1),tokenURIend, {from: account.first});
+					expect(personage).to.be.ok;
+				});
+			})
+		});
 	});
 })
