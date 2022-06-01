74d78b5a218f839991ec24fd916a434c73f0280b
sebastien <sebastiengazeau4@gmail.com>
2022-06-01 15:19:04 +0200

    update migration

--- migrations-ts/2_deploy_contracts.ts
+++ migrations-ts/2_deploy_contracts.ts
@@ -1,7 +1,7 @@
-const Characters = artifacts.require('Characters')
+const StoryFactory = artifacts.require('StoryFactory')
 
 module.exports = function (deployer) {
-  deployer.deploy(Characters, 'Prototype', 'PTT')
+  deployer.deploy(StoryFactory)
 } as Truffle.Migration
 
 // because of https://stackoverflow.com/questions/40900791/cannot-redeclare-block-scoped-variable-in-unrelated-files
